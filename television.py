358e88e454154ece49b34c45c282d09e197cffa1
nicktranuno <nicholastran@unomaha.edu>
2023-11-17 18:22:58 -0600

    Updated television class
    -Removed print('cat') that was used for testing
    
    -Added all class variables for Television
    
    -Added methods for class Television

--- television.py
+++ television.py
@@ -1,1 +1,53 @@
-print('cat')
+
+
+class Television:
+
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.status = False
+        self.muted = False
+        self.volume = self.MIN_VOLUME
+        self.channel = self.MIN_CHANNEL
+
+    def power(self):
+        self.status = not self.status
+
+    def mute(self):
+        self.muted = not self.muted
+
+    def channel_up(self):
+        if self.status:
+            if self.channel < self.MAX_CHANNEL:
+                self.channel += 1
+            else:
+                self.channel = self.MIN_CHANNEL
+
+    def channel_down(self):
+        if self.status:
+            if self.channel > self.MIN_CHANNEL:
+                self.channel -= 1
+            else:
+                self.channel = self.MAX_CHANNEL
+
+    def volume_up(self):
+        if self.status:
+            if self.volume < self.MAX_VOLUME:
+                self.volume += 1
+            else:
+                self.volume = self.MAX_VOLUME
+
+    def volume_down(self):
+        if self.status:
+            if self.volume > self.MIN_VOLUME:
+                self.volume -= 1
+            else:
+                self.volume = self.MIN_VOLUME
+
+    def __str__(self):
+        return f'Power = {self.status}, Channel = {self.channel}, Volume = {self.volume}'
+    
+
